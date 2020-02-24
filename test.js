e1b5fc4ea59a028e71e691ab7bfcc0718c4fb47f
poomwarawat <58084725+poomwarawat@users.noreply.github.com>
2020-02-24 15:47:22 +0700

    teststst

--- test.js
+++ test.js
@@ -1,9 +1,5 @@
 console.log("Hello")
 console.log("Hellooooooooo")
 console.log("Helloasodsaodask;dj;")
-<<<<<<< HEAD
-
 console.log("dfklsdjflkdjfldsklf")
-=======
-console.log("asdklasjdklsajldkasjdlkasjdlkasdjlkas")
->>>>>>> test
+console.log("asdsadasda")
