4300b5e21512316cebcd492ead611bda79a6b9f2
poomwarawat <58084725+poomwarawat@users.noreply.github.com>
2020-02-24 15:56:41 +0700

    ttt

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
 console.log("Hello")
 console.log("Hellooooooooo")
+<<<<<<< HEAD
 console.log("Helloasodsaodask;dj;")
 console.log("dfklsdjflkdjfldsklf")
 console.log("asdsadasda")
 console.log("asdklasjdklsajldkasjdlkasjdlkasdjlkas")
 
 console.log("warawat lailerd")
+=======
+console.log("Helloasodsaodask;dj;")
+>>>>>>> parent of 7179f8b... testsss
